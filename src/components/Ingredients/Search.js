895988dea25c8b3f6d6708a545677b57ce071c90
Karan <karanvknarayanan@gmail.com>
2020-10-22 00:17:15 +0530

    input updated

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
-import Card from '../UI/Card';
-import './Search.css';
+import Card from "../UI/Card";
+import "./Search.css";
 
-const Search = React.memo(props => {
+const Search = React.memo((props) => {
   return (
     <section className="search">
       <Card>
